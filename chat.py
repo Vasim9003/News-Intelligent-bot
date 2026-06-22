bceecc8aad45c14ac9fc3c51b76a7cfdb1debf50
Mohamed Vasim Akram K <107296043+Vasim9003@users.noreply.github.com>
2026-06-22 16:35:40 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,15 +2,13 @@ import google.generativeai as genai
 from pinecone import Pinecone
 from sentence_transformers import SentenceTransformer
 
-# 1. Setup API Keys (Direct-ah paste panniyachu)
+
 PINECONE_KEY = ""
 GEMINI_KEY = ""
 
-# 2. Pinecone Initialize
 pc = Pinecone(api_key=PINECONE_KEY)
 index = pc.Index("news-index")
 
-# 3. Gemini Initialize (IMPORTANT: Key correct-ah pass aaganum)
 genai.configure(api_key=GEMINI_KEY)
 llm = genai.GenerativeModel('gemini-2.5-flash')
 
@@ -20,20 +18,17 @@ embed_model = SentenceTransformer('all-MiniLM-L6-v2')
 def get_answer(user_query):
     print(f"\n🔍 Searching for: {user_query}...")
     
-    # A. Text-ah Vector-ah mathuroom
     query_vector = embed_model.encode(user_query).tolist()
 
     # B. Pinecone search
     results = index.query(vector=query_vector, top_k=2, include_metadata=True)
 
-    # C. Context prepare panroom
     context = ""
     for match in results['matches']:
         title = match['metadata']['title']
         link = match['metadata']['url']
         context += f"\nTitle: {title}\nLink: {link}\n"
 
-    # News kidaikala na general answer kekurom
     if not context:
         prompt = f"The user is asking: {user_query}. I don't have news about this. Answer generally."
     else:
